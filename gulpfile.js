39f5abae11e7be6cca8ccfacf06d9be631efb80c
visdark <visdark@hotmail.com>
2017-03-20 14:49:50 +0800

    初始化项目

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-(function() {
-    'use strict';
+
+    // 声明组件调用
+
     var gulp = require('gulp'),
         cleanCSS = require('gulp-clean-css');
 
-});
+    // 压缩CSS
+    gulp.task('min-css',function() {
+        gulp.src(['./visdark/vis-*.css'])
+            .pipe(cleanCSS())
+            .pipe(gulp.dest('./docs/dist/css/'));
+    });
+
